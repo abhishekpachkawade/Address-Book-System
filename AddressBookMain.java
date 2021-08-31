f4e954cafbba59c942676eb1efacd71029b77e49
abhishekpachkawade <abhishekpachkawade@gmail.com>
2021-08-31 22:01:17 +0530

    Welcome to Address Book Program

---
+++ AddressBookMain.java
@@ -0,0 +1,23 @@
+
+//Start with Displaying Welcome to Address Book Program in AddressBookMain class on Master Branch
+
+package com.addressbook;
+
+public class AddressBookMain {
+
+	public static void main(String[] args) {
+		//print statement 
+		System.out.println("Welcome to Address Book Program");
+
+	}
+
+}
+
+
+/*
+OUTPUT
+
+Welcome to Address Book Program
+
+
+*/
